e4d880774265d382861f4ee0eac7595d4d966c4c
Harsh Koli <harshkoli201@gmail.com>
2020-11-29 21:07:37 +0530

    Added Tables

--- Todo.java
+++ Todo.java
@@ -1,17 +1,35 @@
-import java.awt.*; 
+import java.awt.*;
 import javax.swing.*; 
+import javax.swing.JTable;
+import javax.swing.table.TableModel;
 public class Todo extends JFrame  
 { 
+	JTable jt;
+	JButton jb;
+	JScrollPane sp;
+	
 	Todo()     
 	{         
 		setSize(500,500);
 		setVisible(true);
-		setLayout(new FlowLayout());         
+		setLayout(new GridLayout(2,2,6,6));         
 		JMenuBar mb  = new JMenuBar();         
-		setJMenuBar(mb);         
+		setJMenuBar(mb);
 		JMenu m1  = new JMenu("Add Task");// is for adding a new frame         
-		mb.add(m1);      
-	}     
+		mb.add(m1); 
+		String column[] = {"No","Tasks","Done"};
+		String data[][]={ 	
+						 
+					  
+			
+						};  
+		jt = new JTable(data,column);       
+		sp =new JScrollPane(jt); 
+		add(sp);	
+
+
+	};
+     
 		public static void main(String args[])     
 		{         
 			Todo t = new Todo();         
