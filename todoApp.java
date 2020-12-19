2fa13ca08e3fff27bb7efd40af353450ea0f6cf7
Girish Rajani <girish.r.rajani@gmail.com>
2020-12-20 00:30:51 +0530

    Main window design completed

--- todoApp.java
+++ todoApp.java
@@ -1,18 +1,27 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
-import java.awt.event.ActionListener;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import java.awt.FlowLayout;
+
+import java.awt.event.ActionListener;
+
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
 import java.awt.Color;
+import java.awt.Font;
+
 public class todoApp extends JFrame {    
     JMenuBar mainBar;
     JMenu file,theme;
     JMenuItem addTask,displayTask,exit,dark,light;
-    JLabel welcome;
+    JButton welcome,addTaskButton,displayTaskButton;
+    JPanel buttonPanel;
+    JLabel blankLable;
     todoApp(){
         /*
         This constructor holds the code for the Main Window Which
@@ -21,7 +30,8 @@ public class todoApp extends JFrame {
         setVisible(true);
         setSize(500,350);
         setLayout(new BorderLayout());
-        getContentPane().setBackground(Color.darkGray);
+        // Function to close the operation of JFrame. 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         
         //Menu Bar Code Starts
         mainBar = new JMenuBar();
@@ -43,6 +53,18 @@ public class todoApp extends JFrame {
         //MenuBar Code Ends
 
         //Main Window Code
+        welcome = new JButton("Welcome To Your ToDo App");
+        welcome.setEnabled(false);
+        welcome.setFont(new Font("Helvetica Neue", Font.PLAIN, 20));
+        add(welcome,BorderLayout.NORTH);
+        buttonPanel = new JPanel();
+        addTaskButton = new JButton("ADD TASK");
+        displayTaskButton = new JButton("Display Tasks");
+        buttonPanel.add(addTaskButton);
+        buttonPanel.add(displayTaskButton);
+        buttonPanel.setLayout(new FlowLayout());
+        buttonPanel.setBackground(Color.darkGray); //Default Theme for the App
+        add(buttonPanel,BorderLayout.CENTER);
     }
     
     public static void main(String[] args) {
